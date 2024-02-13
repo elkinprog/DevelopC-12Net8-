efa17802c64bb7ffd53ddb98295a46d15c19f7c8
developer <elkinprog@gmail.com>
2024-02-12 19:45:06 -0500

    first commit

---
+++ AboutMyEnvironment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+
+namespace AboutMyEnvironment;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+     
+        
+        Console.WriteLine($"CurrentDirectory: {Environment.CurrentDirectory}");
+        Console.WriteLine($"VersionString: {Environment.OSVersion.VersionString}");
+        Console.WriteLine($"Namespace: {typeof(Program).Namespace}" );
+        
+        Console.WriteLine(format: "Value is {0}.", arg0: 19.8);
+        
+
+    }
+}
